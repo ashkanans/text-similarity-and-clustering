308ef3f7cb592f023d361258d336e4cd57c2f715
Ashkan Ansarifard <ansarifardashkan@gmail.com>
2024-11-24 17:22:39 +0100

    updates scraper

--- text_similarity/scraping/amazon_scraper.py
+++ text_similarity/scraping/amazon_scraper.py
@@ -4,18 +4,22 @@ from datetime import datetime
 import requests
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 from text_similarity.scraping.data_processing import load_dataset, save_to_tsv
 from text_similarity.scraping.visualization import plot_top_products, scatter_plot
 from utils.utils import get_headers
 
 class AmazonScraper:
-    def __init__(self, keywords, num_pages):
+    def __init__(self, keywords, num_pages, site="amazon.it"):
         self.scraped_results = None
         self.keywords = keywords
         self.num_pages = num_pages
-        self.base_url = "https://www.amazon.it/s"
+        self.site = site
+        self.base_url = f"https://www.{site}/s"
         self.data = []
+
+    from tqdm import tqdm
 
     def scrape_amazon_products(self):
         """
@@ -24,75 +28,102 @@ class AmazonScraper:
         unique_entries = set()  # Track unique products to avoid duplicates
         total_products = 0
         new_products = 0
+        duplicate_products = 0
 
-        for keyword in self.keywords.split(","):
-            keyword = keyword.strip()
-            print(f"Scraping keyword '{keyword}'...")
-            for page in range(1, self.num_pages + 1):
-                url = f"{self.base_url}?k={keyword}&page={page}"
-                headers = get_headers()
-                response = requests.get(url, headers=headers)
-                time.sleep(2)  # Avoid being blocked
+        keywords_list = [keyword.strip() for keyword in self.keywords.split(",")]
+        print(f"{len(keywords_list)} keywords are going to be scraped")
 
-                if response.status_code == 200:
-                    print(f"Scraping page {page}...")
-                    soup = BeautifulSoup(response.text, 'html.parser')
-                    products = soup.find_all('div', {'data-component-type': 's-search-result'})
+        # Progress bar for keywords
+        with tqdm(total=len(keywords_list), desc="Scraping Keywords") as pbar:
+            for keyword in keywords_list:
+                print(f"\nScraping keyword '{keyword}' on {self.site}...")
+                for page in range(1, self.num_pages + 1):
+                    url = f"{self.base_url}?k={keyword}&page={page}"
+                    headers = get_headers()
+                    response = requests.get(url, headers=headers)
+                    time.sleep(2)  # Avoid being blocked
 
-                    for product in products:
-                        try:
-                            product_data = self.extract_product_data(product)
-                            if not product_data:
-                                continue
+                    if response.status_code == 200:
+                        soup = BeautifulSoup(response.text, 'html.parser')
 
-                            # Deduplicate products based on unique attributes
-                            product_tuple = tuple(product_data.values())
-                            if product_tuple in unique_entries:
-                                continue
-                            unique_entries.add(product_tuple)
-                            self.data.append(product_data)
-                            new_products += 1
-                            total_products += 1
-                        except Exception as e:
-                            print(f"Error processing product: {e}")
+                        if self.site == "amazon.it":
+                            products = soup.find_all('div', {'data-component-type': 's-search-result'})
+                        elif self.site == "amazon.com":
+                            products = soup.find_all('div', {'class': 'sg-col-20-of-24 s-result-item'})
 
-                else:
-                    print(f"Failed to retrieve page {page} for keyword '{keyword}'. Status: {response.status_code}")
-                    break
+                        for product in products:
+                            try:
+                                product_data = self.extract_product_data(product)
+                                if not product_data:
+                                    continue
 
-            print(f"Completed scraping for keyword '{keyword}'.")
-        print(f"Scraping completed. Total: {total_products}, New: {new_products}")
+                                # Deduplicate products based on unique attributes
+                                product_tuple = (
+                                    product_data['description'],
+                                    product_data['price'],
+                                    product_data['star_rating'],
+                                    product_data['reviews']
+                                )
+                                if product_tuple in unique_entries:
+                                    duplicate_products += 1
+                                    continue  # Skip adding duplicates
+
+                                unique_entries.add(product_tuple)
+                                self.data.append(product_data)
+                                new_products += 1
+                                total_products += 1
+                            except Exception as e:
+                                print(f"Error processing product: {e}")
+
+                    else:
+                        print(f"Failed to retrieve page {page} for keyword '{keyword}'. Status: {response.status_code}")
+                        break
+
+                print(f"Completed scraping for keyword '{keyword}'.")
+                print(f"{duplicate_products} duplicates found")
+                pbar.update(1)  # Update the progress bar for each completed keyword
+
+        print(f"\nScraping completed. Total: {total_products}, New: {new_products}, Duplicates: {duplicate_products}")
 
     def extract_product_data(self, product):
         """
         Extracts product details from a BeautifulSoup product element.
+        Adapts extraction based on the site structure.
         """
         try:
-            # Extract product description
-            description_element = product.find('span', class_='a-size-base-plus a-color-base a-text-normal')
+            if self.site == "amazon.it":
+                # Extract data for amazon.it
+                description_element = product.find('span', class_='a-size-base-plus a-color-base a-text-normal')
+                url_element = product.find('a', class_='a-link-normal s-no-outline')
+                price_container = product.find('span', class_='a-price')
+                price_element = price_container.find('span', class_='a-offscreen') if price_container else None
+                star_element = product.find('i', class_='a-icon-star-small')
+                review_count_element = product.find('span', class_='a-size-base s-underline-text')
+
+            elif self.site == "amazon.com":
+                # Extract data for amazon.com
+                description_element = product.find('span', class_='a-size-medium a-color-base a-text-normal')
+                url_element = product.find('a', class_='a-link-normal s-underline-text')
+                price_container = product.find('span', class_='a-price')
+                price_element = price_container.find('span', class_='a-offscreen') if price_container else None
+                star_element = product.find('i', class_='a-icon-star-small')
+                review_count_element = product.find('span', class_='a-size-base')
+
             product_description = description_element.text.strip() if description_element else None
+            if "'" in product_description:
+                product_description = product_description.replace("'", "")
+            elif '"' in product_description:
+                product_description = product_description.replace('"', '')
 
-            # Extract product URL
-            url_element = product.find('a', class_='a-link-normal s-no-outline')
             product_url = urljoin(self.base_url, url_element['href']) if url_element else None
-
-            # Extract price
-            price_container = product.find('span', class_='a-price')
-            price_element = price_container.find('span', class_='a-offscreen') if price_container else None
             product_price = (
-                float(price_element.text.strip().replace('.', '').replace(',', '.').replace('€', ''))
+                float(price_element.text.strip().replace('.', '').replace(',', '.').replace('$', '').replace('€', ''))
                 if price_element else None
             )
-
-            # Extract star rating
-            star_element = product.find('i', class_='a-icon-star-small')
             star_rating = (
                 float(star_element.find('span', class_='a-icon-alt').text.split()[0].replace(',', '.'))
                 if star_element else None
             )
-
-            # Extract review count
-            review_count_element = product.find('span', class_='a-size-base s-underline-text')
             num_reviews = (
                 int(review_count_element.text.strip().replace('.', '').replace(',', ''))
                 if review_count_element else None
@@ -101,9 +132,9 @@ class AmazonScraper:
             return {
                 'description': product_description,
                 'price': product_price,
-                'url': product_url,
                 'star_rating': star_rating,
                 'reviews': num_reviews,
+                'url': product_url,
             }
         except Exception as e:
             print(f"Error extracting product data: {e}")
@@ -135,3 +166,4 @@ class AmazonScraper:
 
         # Scatter plot of price vs. star rating
         scatter_plot(df, x='price', y='star_rating', title='Price vs. Star Rating')
+
